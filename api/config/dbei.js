9e73666c44406071a9d5a8db551772e6e01e6ce2
hawik <hawiksyaef@gmail.com>
2022-11-09 13:47:07 +0700

    configure database

--- api/config/dbei.js
+++ api/config/dbei.js
@@ -1,3 +1,8 @@
-import {
-    
-}
+import { Sequelize } from "sequelize";
+
+const dbei = new Sequelize("indah_db", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
+});
+
+export default dbei;
